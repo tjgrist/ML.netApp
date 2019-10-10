a56b7321c8e2de1ea98ad378700150e686b30ef3
Thomas Grist <tgrist@affirma.com>
2019-10-10 16:08:05 -0700

    init

---
+++ MyMLApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MyMLAppML.Model;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            do
+            {
+                var input = new ModelInput();
+                text = Console.ReadLine();
+                input.SentimentText = text;
+
+                // Load model and predict output of sample data
+                ModelOutput result = ConsumeModel.Predict(input);
+                Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+            }
+            while (text != "q");
+            
+        }
+    }
+}
